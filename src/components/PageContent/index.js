bd914a0520505b2eae2bedfae0c831223a3ea224
alexxtomas <atomasllimos@gmail.com>
2023-08-10 13:20:17 +0200

    feat: Add new videos and add meda queries

--- src/components/PageContent/index.js
+++ src/components/PageContent/index.js
@@ -63,20 +63,31 @@ const VIDEOS = [
     chanelName: 'Diaromotor',
     views: '139k visualizaciones',
     date: 'hace 1 año'
+  },
+  {
+    thumbnail:
+      'https://i.ytimg.com/vi/OEWDDQe_L0g/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLA2Fuia3jmRD3XqQmulsqCLlszWEw',
+    duration: '14:48',
+    chanelImage:
+      'https://yt3.ggpht.com/ytc/AOPolaTEMxnvpmhvtntW6jR6ZbGN91OUQIgo1sNp81Kiag=s68-c-k-c0x00ffffff-no-rj',
+    title:
+      'BMW M3 Touring Vs. Audi RS4 Avant: no al SUV, ¿cuál es el MEJOR familiar DEPORTIVO? | Diariomotor',
+    chanelName: 'Diaromotor',
+    views: '139k visualizaciones',
+    date: 'hace 1 año'
   }
 ]
 
-const PageContent = ({ tags }) => {
+const PageContent = ({ tags, results }) => {
   return /* html */ `
    <main>
-    ${PageHeader()}
+    ${PageHeader({ results })}
     <section class="page-content-section">
     ${TagSlider({ tags })}
     </section>
     <section class="videos-layout">
       ${VIDEOS.map((video) => VideoCard(video)).join('')}
     </section>
-
     </main>
   `
 }
