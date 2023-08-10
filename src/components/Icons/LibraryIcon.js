038805ef6d2de3f18c1f145cd669710dd9a04bff
alexxtomas <atomasllimos@gmail.com>
2023-08-10 13:20:30 +0200

    chore: Change Icon

--- src/components/Icons/LibraryIcon.js
+++ src/components/Icons/LibraryIcon.js
@@ -1,6 +1,6 @@
 const LibraryIcon = () => {
   return /* html */ `
-  <svg fill="#fff" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block;"><path d="M4 20h14v1H3V6h1v14zM21 3v15H6V3h15zm-4 7.5L11 7v7l6-3.5z"></path></svg>
+  <svg fill="#fff" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; "><path d="m11 7 6 3.5-6 3.5V7zm7 13H4V6H3v15h15v-1zm3-2H6V3h15v15zM7 17h13V4H7v13z"></path></svg>
   `
 }
 
